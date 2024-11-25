f6750b473d5648ef570e515b67f0cf4070f13c0f
mantzaris <avmantzaris@gmail.com>
2024-11-25 12:38:36 -0500

    instantiating the project

---
+++ src/BenchmarkDataNLP.jl
@@ -0,0 +1,43 @@
+module BenchmarkDataNLP
+
+    export greet, add_random_suffix, repeat_string
+
+    using Random
+
+    """
+        greet()
+
+    Prints a friendly greeting message to the console.
+    """
+    greet() = println("Hello World! Yours Truly, B.D.NLP")
+
+    """
+        add_random_suffix(input::AbstractString) -> String
+
+    Takes a string and returns it with a random 5-character suffix added.
+
+    # Examples
+
+        julia> add_random_suffix("hello")
+        "helloabc12"
+    """
+    function add_random_suffix(input::AbstractString)
+        random_suffix = randstring(5)
+        return input * random_suffix
+    end
+
+    """
+        repeat_string(input::AbstractString, times::Int) -> String
+
+    Repeats a string or character a specified number of times.
+
+    # Examples
+
+        julia> repeat_string("hi", 3)
+        "hihihi"
+    """
+    function repeat_string(input::AbstractString, times::Int)
+        return repeat(input, times)
+    end
+
+end  # module BenchmarkDataNLP
