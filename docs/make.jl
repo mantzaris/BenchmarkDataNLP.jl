0c0cdec8f7786bcd4a18fa29cf143e52c24e4756
mantzaris <avmantzaris@gmail.com>
2024-11-26 11:17:31 -0500

    updates to the build of the docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,8 @@ using BenchmarkDataNLP
 
 makedocs(
     sitename = "BenchmarkDataNLP",
-    format = Documenter.HTML(),
+    format =  Documenter.HTML(),
     modules = [BenchmarkDataNLP],
-    repo = "https://github.com/mantzaris/BenchmarkDataNLP.jl",
-    url = "https://mantzaris.github.io/BenchmarkDataNLP.jl/",
-    baseurl = "/BenchmarkDataNLP.jl"
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
