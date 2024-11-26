65791cb86459dfbb76bcbdd29e71fd32ecf7ae17
mantzaris <avmantzaris@gmail.com>
2024-11-26 10:52:31 -0500

    updates to the documentation.yml and make.jl for the docs

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,4 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(
-    repo = "github.com/mantzaris/BenchmarkDataNLP.jl.git"
-)
+deploydocs()
